7d0ecbaa72988c562d407e39eb4ef803ebcb46ab
ignaciobabich <ifernandezbabich@udesa.edu.ar>
2026-03-30 16:26:16 -0300

    Hice el Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+ 
+function Footer(){
+    return (
+        <React.Fragment>
+            <nav>
+            <ul class="nav nav-tabs my-4">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Tommy Smaevich</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="movies.html">Ignacio Fernandez Babich</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="series.html">Luca Blejer</a>
+                </li>
+            </ul>
+        </nav>
+        </React.Fragment>
+    )
+}
+
+export default Footer
