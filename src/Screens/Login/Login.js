ca4151a8113d9d6cd8bdc4e5198e8f2450e52072
ignaciobabich <ifernandezbabich@udesa.edu.ar>
2026-04-06 17:33:37 -0300

    Hice cambios dentro del login

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Login(){
+class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email:'', contraseña:''
+        }
+    }
+
+evitarSubmit(event){
+    event.preventDefault();
+}
+
+controlarCambios(event){
+    this.setState({valor: event.target.value})
+}
+
+render(){
     return(
-        <ReactFragment>
-            <div class="row justify-content-center">
-            <div class="col-md-6">
-                <form>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Ingresá tu email"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Contraseña</label>
-                        <input type="password" class="form-control" id="password" placeholder="Ingresá tu contraseña"/>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
-                </form>
-                <p class="mt-3 text-center">¿No tenés cuenta? <a href="register.html">Registrarse</a></p>
-            </div>
-        </div>
-        </ReactFragment>
+        <form onSubmit={(event)=> this.evitarSubmit(event)}>
+
+        </form>
     )
+}
+
+// render(){
+//     return(
+//         <ReactFragment>
+//             <div class="row justify-content-center">
+//             <div class="col-md-6">
+//                 <form>
+//                     <div class="form-group">
+//                         <label for="email">Email</label>
+//                         <input type="email" class="form-control" id="email" placeholder="Ingresá tu email"/>
+//                     </div>
+//                     <div class="form-group">
+//                         <label for="password">Contraseña</label>
+//                         <input type="password" class="form-control" id="password" placeholder="Ingresá tu contraseña"/>
+//                     </div>
+//                     <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
+//                 </form>
+//                 <p class="mt-3 text-center">¿No tenés cuenta? <a href="register.html">Registrarse</a></p>
+//             </div>
+//         </div>
+//         </ReactFragment>
+//     )
+//}
+
+
+
 }
 
 export default Login
