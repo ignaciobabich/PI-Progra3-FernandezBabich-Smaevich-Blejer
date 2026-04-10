9642dd938a235b976b89447140274c182e4f3950
ignaciobabich <ifernandezbabich@udesa.edu.ar>
2026-04-10 15:17:02 -0300

    modifique el Home, y estoy haciendo la api

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import Header from '../../Components/Header/Header';
-import Footer from '../../Components/Footer/Footer';
-import SearchBar from '../../Components/SearchBar/SearchBar'
+import React, { Component } from 'react';
 
-function Register(){
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar />
-      <SeccionPeliculas/>
-      <SeccionPeliculas/>
-      <CardsForTVShows/>
-      <CardsForTVShows/>
-      <Footer />
-    </div>
-  );
+
+
+
+class Home extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            datos: []
+        }
+    }
+    componentDidMount(){
+        fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=6ee91af43dc9c7cc853f7185e80bbf53')
+        .then(Response => Response.json())
+        .then(data => this.setState(
+            {datos: data.results}
+        ))
+        .catch(error => console.log(error));
+    }
+    render(){
+        return(
+            <>
+               { this.state.datos.length === 0 ? <h3>Cargar...</h3> :
+               
+                this.state.datos.map(() => )
+               }
+
+            </>
+        )
+    }
 }
+
+export default Home
