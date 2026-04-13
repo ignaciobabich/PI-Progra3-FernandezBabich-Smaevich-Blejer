b2d553f33b99b779052b5adf047569e2a13598dc
TommySma <tsmaevicb8@gmail.com>
2026-04-13 15:51:41 -0300

    intento arreglar el ver mas

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -27,16 +27,16 @@ class Card extends Component {
     render() {
         return(
             <article class="single-card-movie">
-                <img src="https://image.tmdb.org/t/p/w500/tzrJulItjttxzoX0t3B2My46TS7.jpg" class="card-img-top"
-                    alt="..."/>
+                <img src={this.props.imagen} class="card-img-top" alt="..."/>
                 <div class="cardBody">
-                    <h5 class="card-title">The Thursday Murder Club</h5>
-                    <p class="card-text">A group of senior sleuths passionate about solving cold cases get plunged into
-                        a real-life murder mystery in this comic crime caper.</p>
-                    <a href="movie.html" class="btn btn-primary">Ver más</a>
+                    <h5 class="card-title">{this.props.nombre}</h5>
+                    <button className='more' onClick={() => this.cambiarEstado()}>{this.state.textoBoton}</button>
+                        <section className='extra'>
+                            <a href="Detalle.js" className={this.state.ClaseOculta}>{this.props.descripcion}</a>
+                        </section>
                     <a href="" class="btn alert-primary">🩶</a>
                 </div>
-            </article>
+                </article>
         )
     }
 }
