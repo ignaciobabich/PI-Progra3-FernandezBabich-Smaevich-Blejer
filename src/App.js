ee7efa01f0dee859c23b832f568cd0f69d91f9f3
ignaciobabich <ifernandezbabich@udesa.edu.ar>
2026-04-17 15:23:27 -0300

    por terminar el NotFound

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from './Components/Header/Header';
+//import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import SearchBar from './Components/SearchBar/SearchBar';
-import CardsForTVShows from './Components/CardsForTVShows/CardsForTVShows';
+//import SearchBar from './Components/SearchBar/SearchBar';
+//import CardsForTVShows from './Components/CardsForTVShows/CardsForTVShows';
 import Home from './Screens/Home/Home';
-import SectionPelis from './Components/SectionPelis/SectionPelis'
-import TVShows from './Components/TVShows/TVShows';
+//import SectionPelis from './Components/SectionPelis/SectionPelis'
+//import TVShows from './Components/TVShows/TVShows';
+//import NotFound from './Screens/NotFound/NotFound';
 
 
 function App() {
@@ -18,3 +19,5 @@ function App() {
 }
 
 export default App;
+
+// falta poner el switch con todas las rutas y linkear todo
--- src/Screens/NotFound/NotFound.js
+++ src/Screens/NotFound/NotFound.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 function NotFound(){
     return(
-        
+       <div ClassName= "cards cardBody">
+        <h1>404 - contenido inexistente</h1>
+        <Link to="/"></Link>
+       </div> 
     )
 }
+
+export default NotFound;
