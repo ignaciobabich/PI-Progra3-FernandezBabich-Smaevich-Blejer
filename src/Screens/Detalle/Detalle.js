e0c47bb964016e75f7c734dd6724e13a0486d271
TommySma <tsmaevicb8@gmail.com>
2026-04-20 17:46:36 -0300

    arreglando detalle

--- src/Screens/Detalle/Detalle.js
+++ src/Screens/Detalle/Detalle.js
@@ -2,20 +2,24 @@ import React, { Component } from "react";
 import CardDetalle from '../../Components/CardDetalle/CardDetalle'
 
 class Detalle extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            peliculas: [
-
-            ]
+            peliculas: [],
+            cargando: true
         };
     }
 
     componentDidMount() {
-        fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=6ee91af43dc9c7cc853f7185e80bbf53')
+
+        const tipo = this.props.match.params.tipo;
+        const id = this.props.match.params.id;
+
+        fetch('https://api.themoviedb.org/3/${tipo}/${id}?api_key=6ee91af43dc9c7cc853f7185e80bbf53')
             .then(response => response.json())
             .then(data => {this.setState ({
-                    peliculas: data.results.slice(0,1)
+                    peliculas: data,
+                    cargando: false
                 });
             })
             .catch((error) => console.log(error));
@@ -24,22 +28,25 @@ class Detalle extends Component {
    
 
     render() {
+        const tipo = this.props.match.params.tipo;
     return (
       <React.Fragment>
       
         <section className="cards" id="movies">
-        {this.state.peliculas.length === 0 ? (<h3>Cargando...</h3>) : (
-             this.state.peliculas.map((peli) => 
+        {this.state.cargando ? (<h3>Cargando...</h3>) : (
             <CardDetalle
-              key={peli.id}
-              id={peli.id}
-              nombre={peli.title}
-              imagen={peli.poster_path}
-              descripcion={peli.overview}
-              date={peli.release_date}
-              puntuacion={peli.vote_average}
+              key={this.state.peliculas.id}
+              id={this.state.peliculas.id}
+              tipo = {tipo}
+              nombre={tipo === "movie" ? this.state.peliculas.title : this.state.item.name}
+              imagen={this.state.peliculas.poster_path}
+              descripcion={this.state.peliculas.overview}
+              date={tipo === "movie" ? this.state.peliculas.release_date : this.state.peliculas.first_air_date}
+              puntuacion={this.state.peliculas.vote_average}
+              duracion = {this.state.peliculas.runtime}
+              generos={this.state.peliculas.genres}
             />
-          )
+          
         )}
       </section>
       </React.Fragment>
